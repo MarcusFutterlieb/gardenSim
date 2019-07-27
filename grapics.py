6095ca21c1b6f7b3f8d693f378e9a5e49ea7a7b9
Marcus Futterlieb <16731186+MarcusFutterlieb@users.noreply.github.com>
2019-07-27 13:37:09 +0200

    creaded graphics module

---
+++ grapics.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 26 2019
+
+@author: Marcus Futterlieb
+"""
+
+
+from turtle import * # imports the module turtle,
+                    #* stands for all, which makes things easier
